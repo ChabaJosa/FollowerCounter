d5f289e670d3777eeaddcf1c1f4ee079278d3f1d
nancy <nancy@RADAR4.local>
2019-11-14 11:54:30 -0500

    Managed to get the exact follower number + CleanUp

--- FollowerCounter.py
+++ FollowerCounter.py
@@ -12,21 +12,12 @@ class FollowerCounter:
         self.browser.get(link)
         time.sleep(3)
 
-        # elements = self.browser.find_elements_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span')
         elements = self.browser.find_elements_by_class_name('g47SY')
         element_attribute_value = elements[1].get_attribute('title')
-        #Add it somewhere, maybe a variable 
         print(element_attribute_value)
 
         time.sleep(3)
         self.browser.close()
-
-    ##def cleanUp (self, pattern):
-        #Regex for followers
-        #re.comppile 
-
-    #def print (self):
-        #print(cssClass)
  
 if __name__ == '__main__':
     follower_counter = FollowerCounter('https://www.instagram.com/radiorochela/')
